71626991fc4818946f1936bae0a6601f29828da7
fynyky <iygnohil@gmail.com>
2022-07-09 08:35:00 +0000

    Got basic db connection working

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -1,54 +1,49 @@
 // -----------------------------------------------------------------------------
 // Dependencies
 // -----------------------------------------------------------------------------
-import express from 'express';
-import url from 'url';
-import path from "path";
+import express from 'express'
+import url from 'url'
+import path from "path"
+import pg from 'pg'
+
 // -----------------------------------------------------------------------------
 // Environmental Variables && Constants
 // -----------------------------------------------------------------------------
-const PORT = process.env.PORT ? process.env.PORT : 1337;
-const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : "db";
-const DB_PORT = process.env.DB_PORT ? process.env.DB_PORT : 5432;
-const DB_NAME = process.env.DB_NAME ? process.env.DB_NAME : "postgres";
-const DB_USER = process.env.DB_USER ? process.env.DB_USER : "postgres";
-const DB_PASSWORD = process.env.DB_PASSWORD ? process.env.DB_PASSWORD : "postgres";
+const PORT = process.env.PORT ? process.env.PORT : 1337
+const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : "db"
+const DB_PORT = process.env.DB_PORT ? process.env.DB_PORT : 5432
+const DB_NAME = process.env.DB_NAME ? process.env.DB_NAME : "postgres"
+const DB_USER = process.env.DB_USER ? process.env.DB_USER : "postgres"
+const DB_PASSWORD = process.env.DB_PASSWORD ? process.env.DB_PASSWORD : "postgres"
 
 // -----------------------------------------------------------------------------
 // Initialization
 // -----------------------------------------------------------------------------
+const pool = new pg.Pool({
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD
+})
 // Setup the main application stack
-const app = express();
-app.use
+const app = express()
 // Find the path to the staic file folder
-const filePath = url.fileURLToPath(import.meta.url);
-const serverPath = path.dirname(filePath);
+const filePath = url.fileURLToPath(import.meta.url)
+const serverPath = path.dirname(filePath)
 const publicPath = path.join(serverPath, "public"); 
 
 // -----------------------------------------------------------------------------
 // Web Server
 // -----------------------------------------------------------------------------
-app.use(express.static(publicPath));
-app.get("/:route", (request, response) => {
-  response.send(`postgres-node-dev-template: server.js ${request.params.route}`);
-});
+app.use(express.static(publicPath))
 
-// -----------------------------------------------------------------------------
-// Postgres testing stuff
-// -----------------------------------------------------------------------------
-// import pg from 'pg'
-// const client = new pg.Client({
-//   host: DB_HOST,
-//   port: DB_PORT,
-//   database: DB_NAME,
-//   user: DB_USER,
-//   password: DB_PASSWORD
-// })
-// await client.connect()
-// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-// console.log(res.rows[0].message) // Hello world!
+app.get("/:route", async (request, response) => {
+  const result = await pool.query('SELECT $1::text as message', [`Hello ${request.params.route}!`])
+  response.send(`postgres-node-dev-template: server.js ${result.rows[0].message}`)
+});
 
 // -----------------------------------------------------------------------------
 // Deployment
 // -----------------------------------------------------------------------------
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
