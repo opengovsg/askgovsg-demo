d0c7c6ec52ec4f5f0068ee39e5a3b25bf03eb6d1
fynyky <iygnohil@gmail.com>
2022-07-10 10:25:59 +0700

    Updated model and sample code

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,15 @@
 -- Initializes the app data models in the database
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE account
-(
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name text
+
+CREATE TABLE account (
+  account_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  account_name text,
+  account_created_at timestamp default current_timestamp
+);
+
+CREATE TABLE post (
+  post_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
+  post_owner_id uuid REFERENCES account(id),
+  post_description text,
+  post_created_at timestamp default current_timestamp
 );
